4355abdbeabcb2c1623974809996e386655dd26f
LilAtza <ysaacnoe.correa@unmsm.edu.pe>
2024-05-05 17:32:26 -0500

    ✨ (entity): message entity class

---
+++ lib/domain/entities/message.dart
@@ -0,0 +1,19 @@
+
+
+enum FromWho{
+  me,
+  bot,
+}
+
+class Message {
+  final String text;
+  final String? imageUrl;
+  final FromWho fromWho;
+
+  Message({
+    required this.text,
+    this.imageUrl,
+    required this.fromWho
+  });
+
+}
